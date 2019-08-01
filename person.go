d51cabffd2487f249a21f40aa20b570a38cdf5c0
itslearninggermany <thomas.nordmann@itslearning.com>
2019-08-01 13:03:34 +0200

    added password

--- person.go
+++ person.go
@@ -1,5 +1,5 @@
 // Name: Thomas Nordmann (thomas.nordmann@itslearning.com)
-// Date: 13.01.2019
+// Date: 01.08.2019
 // Function of the package: Userprovisioning for IMS-E
 // Function of the file:
 
@@ -21,8 +21,12 @@ type person struct {
 		Source string `xml:"source"`
 		ID     string `xml:"id"` // Synchronisationkey
 	} `xml:"sourcedid"`
-	Userid string `xml:"userid"` // USERNAME
-	Name   struct {
+	Userid struct {
+		Value    string `xml:",chardata"` // USERNAME
+		Password string `xml:"password,attr"`
+	} `xml:"userid"`
+
+	Name struct {
 		Fn string `xml:"fn"`
 		N  struct {
 			Family string `xml:"family"`
@@ -49,12 +53,14 @@ type person struct {
 }
 
 // Creates a new Person
-func Person(institution, syncID, username, givenname, familyname, birthday, email, street, locality, pcode, kindOfPersonStudentOrStaffOrParent string, tel []telefon, childOrParentIds []string, parent bool) person {
+func Person(institution, syncID, username, password, givenname, familyname, birthday, email, street, locality, pcode, kindOfPersonStudentOrStaffOrParent string, tel []telefon, childOrParentIds []string, parent bool) person {
 	a := new(person)
 	a.Recstatus = "1"
 	a.Sourcedid.Source = institution
 	a.Sourcedid.ID = syncID
-	a.Userid = username
+	a.Userid.Value = username
+	a.Userid.Password = password
+
 	a.Name.Fn = givenname + " " + familyname
 	a.Name.N.Family = familyname
 	a.Name.N.Given = givenname
@@ -77,10 +83,10 @@ func Person(institution, syncID, username, givenname, familyname, birthday, emai
 func (a *person) ParseToXML() ([]byte, string, error) {
 	output, err := xml.Marshal(a)
 	if err != nil {
-		err2 := errors.New(err.Error() + " Person: " + a.Userid + " was not(!!!) imported. \n")
+		err2 := errors.New(err.Error() + " Person: " + a.Userid.Value + " was not(!!!) imported. \n")
 		return output, "", err2
 	}
-	log := "Person: " + a.Userid + " is imported. \n"
+	log := "Person: " + a.Userid.Value + " is imported. \n"
 
 	return output, log, err
 }
@@ -92,15 +98,15 @@ func (a *person) ParseToXML() ([]byte, string, error) {
 func (a *person) Save2File (pathname string) (oldPersonDeletet bool) {
 oldPersonDeletet = false;
 if (IsFileInTheDirectory (pathname, a.Userid + ".xml")) {
-	os.Remove(pathname+a.Userid+".xml");
-	oldPersonDeletet = true;
+   os.Remove(pathname+a.Userid+".xml");
+   oldPersonDeletet = true;
 }
 
 file, err := os.Create(pathname+a.Userid+".xml")
 _, err = file.Write(a.ParseToXML())
 if err != nil {
-	fmt.Println(err)
-	return
+   fmt.Println(err)
+   return
 }
 return
 
@@ -124,40 +130,40 @@ func (a *person) Inmport2Database (nameOfTheDatabase string) error{
 db := ConnectionZuDB(nameOfTheDatabase);
 defer db.Close()
 
-	stmtOut, err := db.Prepare("SELECT COUNT(idInstitution) FROM Institution WHERE name = ?;")
-	if err != nil {
-		return err
-	}
-	defer stmtOut.Close()
-	var institutionid int
+   stmtOut, err := db.Prepare("SELECT COUNT(idInstitution) FROM Institution WHERE name = ?;")
+   if err != nil {
+      return err
+   }
+   defer stmtOut.Close()
+   var institutionid int
 
 // First get the institutionid, if there is no, a new one will be created in the database.
-	rows, err = stmtOut.QueryRow(a.Sourcedid.Source).Scan(&institutionid)
-	if err != nil {
-		return err
-	}
+   rows, err = stmtOut.QueryRow(a.Sourcedid.Source).Scan(&institutionid)
+   if err != nil {
+      return err
+   }
 
 
-	stmt , err := db.Prepare("INSERT INTO Institution (name) VALUE (?)")
-		if err != nil {
-			return err
-		}
-		_,err = stmt.Exec(a.Sourcedid.Source)
+   stmt , err := db.Prepare("INSERT INTO Institution (name) VALUE (?)")
+      if err != nil {
+         return err
+      }
+      _,err = stmt.Exec(a.Sourcedid.Source)
 
-		return err
-		stmt.Close()
+      return err
+      stmt.Close()
 
-		stmtOut2, err := db.Prepare("SELECT idInstitution FROM Institution WHERE name = ?;")
-		if err != nil {
-			return err
-		}
-		defer stmtOut2.Close()
+      stmtOut2, err := db.Prepare("SELECT idInstitution FROM Institution WHERE name = ?;")
+      if err != nil {
+         return err
+      }
+      defer stmtOut2.Close()
 
-		err = stmtOut2.QueryRow(a.Sourcedid.Source).Scan(&institutionid)
-		if err != nil {
-			return err
-		}
-	}
+      err = stmtOut2.QueryRow(a.Sourcedid.Source).Scan(&institutionid)
+      if err != nil {
+         return err
+      }
+   }
 
 // Second: Create a new idPerson, which one is unique
 rand.Seed(time.Now().UTC().UnixNano())
@@ -166,7 +172,7 @@ rand.Seed(time.Now().UTC().UnixNano())
 // Third: Import the person to the database
 stmt , err := db.Prepare("INSERT INTO Person (idPerson, idInstitution, username, givenname, familyname, birthday, email, street, locality, pcode, kindOfPersonStudentOrStaffOrParent,synckey) VALUE (?,?,?,?,?,?,?,?,?,?,?,?)")
 if err != nil {
-	return err
+   return err
 }
 defer stmt.Close()
 
@@ -176,7 +182,7 @@ idPerson :=  rand.Intn(2147483647)
 _,err = stmt.Exec(idPerson,institutionid, a.Userid, a.Name.N.Given, a.Name.N.Family, a.Demographics.Bday, a.Email, a.Adr.Street,a.Adr.Locality, a.Adr.Pcode, a.Institutionrole.Institutionroletype,a.Sourcedid.ID)
 
 if (err != nil) {
-	return err
+   return err
 }
 
 fmt.Println("Person with username" + a.Userid + "was imported to the database")
@@ -187,16 +193,16 @@ return nil
 
 func GetPersonFromDatabase (UserID string) person {
 
-	rows , err := db.Query("SELECT * FROM Person;")
-	//_, err = db.Query("INSERT INTO Person (idPerson, idInstitution, idUser, givenname, familyname, birthday, email, street, locality, pcode, kindOfPersonStudentOrStaffOrParent) VALUE  (266333,1,123499956,'Felicitas','Nordmann','17.01.2015','cecilia@mail.de','Keithstraße 29','Berlin','10787','student');")
-	defer rows.Close()
-	for rows.Next() {
-		err := rows.Scan( , &name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(id, name)
-	}
+   rows , err := db.Query("SELECT * FROM Person;")
+   //_, err = db.Query("INSERT INTO Person (idPerson, idInstitution, idUser, givenname, familyname, birthday, email, street, locality, pcode, kindOfPersonStudentOrStaffOrParent) VALUE  (266333,1,123499956,'Felicitas','Nordmann','17.01.2015','cecilia@mail.de','Keithstraße 29','Berlin','10787','student');")
+   defer rows.Close()
+   for rows.Next() {
+      err := rows.Scan( , &name)
+      if err != nil {
+         log.Fatal(err)
+      }
+      log.Println(id, name)
+   }
 
 
 }
